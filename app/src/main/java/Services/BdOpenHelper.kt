4d6820d71380fdfd3f80fa0c650f28e94e96e1b3
Carlos Ernesto González Flores <cgf72793@udelasalle.edu.mx>
2022-09-30 16:57:38 -0500

    CarlosErnestoGonzalezFlores_CorrecionUsuario_ClienteYCreacionTabla

--- app/src/main/java/Services/BdOpenHelper.kt
+++ app/src/main/java/Services/BdOpenHelper.kt
@@ -22,9 +22,18 @@ class BdOpenHelper(context: Context): SQLiteOpenHelper(context,DATABASE_NAME,nul
         val INT_TYPE = "integer"
 
         val CREATE_EVENTOS_SCRIPT = (
-                "create table c_usuario (" +
-                        " id_usuario " + INT_TYPE + " primary key autoincrement," +
-                        " nb_usuario " + STRING_TYPE + " not null," +
-                        " cl_usuario" + STRING_TYPE + " not null)")
+                "create table cliente (" +
+                        " id_cliente " + INT_TYPE + " primary key autoincrement," +
+                        " nombre_cliente " + STRING_TYPE + " not null," +
+                        " usuario_cliente" + STRING_TYPE + " not null)" +
+                        " cp_cliente " + STRING_TYPE + " not null," +
+                        " telefono_cliente" + STRING_TYPE + " not null)" +
+                        " correo_cliente " + STRING_TYPE + " not null," +
+                        " contrasenia_cliente" + STRING_TYPE + " not null)" +
+                        " foto_cliente " + STRING_TYPE + " not null," +
+                        " genero_cliente" + STRING_TYPE + " not null)" +
+                        " fechanac_cliente " + STRING_TYPE + " not null," +
+                        " apellido_cliente" + STRING_TYPE + " not null)"
+                )
     }
 }
