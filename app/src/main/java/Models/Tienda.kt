9b00dadb1425042792a140b3572de7913b9709b9
andresxordero <andresmcorderor@gmail.com>
2022-10-11 14:29:22 -0500

    AndresManuelCorderoRamirez_TablasSQLiteYClaseTienda

--- app/src/main/java/Models/Tienda.kt
+++ app/src/main/java/Models/Tienda.kt
@@ -2,24 +2,24 @@ package com.amcr.appforum
 
 import Models.Usuario
 
-class Tienda : Usuario {
-
+open class Tienda : Usuario {
     //Propiedades clase Tienda
-    var idTienda:Number = 0
+    var idTienda: Number = 0
     var Categoria: String = ""
-    var Etiquetas = arrayOf<String>()
-    var Seguidores: Int = 0
+    lateinit var Etiquetas:List<String>
+    var Seguidores: Number = 0
     var Puntuacion: Double = 0.0
     var Envios: Boolean = false
 
     //Constructores
     // Constructor Eliminar
-    constructor(ID:Number) :super(ID){
+    constructor(Id:Number): super(Id) {
+        this.idTienda=Id
     }
 
     // Constructor Actualizar
-    constructor(Nombre: String, Apellidos: String, Usuario: String, CP: String, Telefono: String, Correo: String, Contrasenia: String, FotoDePerfil: String,categoria:String, etiquetas:Array<String>, seguidores:Int, puntuacion:Double, envios:Boolean)
-            :super(Nombre, Apellidos, Usuario, CP, Telefono, Correo, Contrasenia, FotoDePerfil){
+    constructor(Nombre: String, Usuario: String, CP: String, Telefono: String, Correo: String, Contrasenia: String, FotoDePerfil: String,categoria: String, etiquetas:List<String>, seguidores: Number, puntuacion: Double, envios: Boolean)
+            :super(Nombre, Usuario, CP, Telefono, Correo, Contrasenia, FotoDePerfil){
         this.Categoria=categoria
         this.Etiquetas=etiquetas
         this.Seguidores=seguidores
@@ -28,8 +28,8 @@ class Tienda : Usuario {
     }
 
     // Constructor Crear
-    constructor(ID:Number,Nombre: String, Apellidos: String, Usuario: String, CP: String, Telefono: String, Correo: String, Contrasenia: String, FotoDePerfil: String, idTienda:Number,categoria:String, etiquetas:Array<String>, seguidores:Int, puntuacion:Double, envios:Boolean)
-            :super(ID,Nombre, Apellidos, Usuario, CP, Telefono, Correo, Contrasenia, FotoDePerfil){
+    constructor(ID:Number,Nombre: String, Usuario: String, CP: String, Telefono: String, Correo: String, Contrasenia: String, FotoDePerfil: String, categoria: String, etiquetas:List<String>, seguidores: Number, puntuacion: Double, envios: Boolean)
+            :super(ID,Nombre, Usuario, CP, Telefono, Correo, Contrasenia, FotoDePerfil){
         this.idTienda=ID
         this.Categoria=categoria
         this.Etiquetas=etiquetas
--- app/src/main/java/Models/Usuario.kt
+++ app/src/main/java/Models/Usuario.kt
@@ -2,9 +2,8 @@ package Models
 
 open class Usuario{
     //Props
-    private var ID:Number = 0
+    private var ID: Number = 0
     private var Nombre: String = ""
-    private var Apellidos: String = ""
     private var Usuario: String = ""
     private var CP: String = ""
     private var Telefono: String = ""
@@ -14,10 +13,8 @@ open class Usuario{
 
     // Constructs
     // Constructor Crear
-    constructor(ID:Number, Nombre: String, Apellidos: String, Usuario: String, CP: String, Telefono: String, Correo: String, Contrasenia: String, FotoDePerfil: String){
-        this.ID=ID
+    constructor(Nombre: String, Usuario: String, CP: String, Telefono: String, Correo: String, Contrasenia: String, FotoDePerfil: String){
         this.Nombre=Nombre
-        this.Apellidos=Apellidos
         this.Usuario=Usuario
         this.CP=CP
         this.Telefono=Telefono
@@ -26,9 +23,9 @@ open class Usuario{
         this.FotoDePerfil=FotoDePerfil
     }
     // Constructor Actualizar
-    constructor(Nombre: String, Apellidos: String, Usuario: String, CP: String, Telefono: String, Correo: String, Contrasenia: String, FotoDePerfil: String){
+    constructor(ID: Number,Nombre: String, Usuario: String, CP: String, Telefono: String, Correo: String, Contrasenia: String, FotoDePerfil: String){
+        this.ID=ID
         this.Nombre=Nombre
-        this.Apellidos=Apellidos
         this.Usuario=Usuario
         this.CP=CP
         this.Telefono=Telefono
@@ -37,9 +34,8 @@ open class Usuario{
         this.FotoDePerfil=FotoDePerfil
     }
     // Constructor Eliminar
-    constructor(ID:Number){
-        this.ID=ID
+    constructor(id:Number) {
+        this.ID = id
     }
+    // Métodos
 }
-
-// Métodos
