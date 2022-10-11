49fcb171fb39dc153325b6d3011eb84c35772eb4
Ricardo GF <ricardogf396@gmail.com>
2022-10-11 14:15:09 -0500

    Ricardo Gonzalez Flores - Creacion Fotos_Publicacion

--- app/src/main/java/Models/Publicacion.kt
+++ app/src/main/java/Models/Publicacion.kt
@@ -1,41 +1,16 @@
 package Models
 
-import java.util.*
+class Publicacion {
 
-open class Publicacion {
+    val idPublicacion : Int;
+    var nombre:String;
+    var descripcion:String;
+    var precio:Double;
+    var fotos:List<String>;
+    var fecha:String;
+    var categoria:String;
+    var tipo:String;
 
-    private var idPublicacion : Int = 0;
-    private var nombre:String = "";
-    private var descripcion:String = "";
-    private var precio:Double = 0.0;
-    private var fotos:List<String> = Arrays.asList("");
-    private var fecha:String = "";
-    private var categoria:String = "";
-    private var tipo:String = "";
-
-    // constructor crear
-    constructor(
-        idPublicacion: Int,
-        nombre: String,
-        descripcion: String,
-        precio: Double,
-        fotos: List<String>,
-        fecha: String,
-        categoria: String,
-        tipo: String,
-    ){
-        this.idPublicacion = idPublicacion
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.fotos = fotos;
-        this.fecha = fecha;
-        this.categoria = categoria;
-        this.tipo = tipo;
-
-    }
-
-    // constructor actualizar
     constructor(
         nombre: String,
         descripcion: String,
@@ -44,19 +19,15 @@ open class Publicacion {
         fecha: String,
         categoria: String,
         tipo: String,
-    ){
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.fotos = fotos;
-        this.fecha = fecha;
-        this.categoria = categoria;
-        this.tipo = tipo;
-    }
-// Constructor eliminar
-    constructor(
         idPublicacion: Int
     ){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.fotos = fotos;
+        this.fecha = fecha;
+        this.categoria = categoria;
+        this.tipo = tipo;
         this.idPublicacion = idPublicacion
     }
 
